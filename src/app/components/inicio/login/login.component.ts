006d3c842a92130bf5c7552b455a517cba90449e
joel <libra-marriaga@hotmailt.com>
2023-01-09 12:03:03 -0500

    register component terminado

--- src/app/components/inicio/login/login.component.ts
+++ src/app/components/inicio/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Usuario } from '../../../models/usuario';
 
 @Component({
   selector: 'app-login',
@@ -7,4 +9,21 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
 
+  login: FormGroup
+
+  constructor(private fb: FormBuilder) {
+    this.login = this.fb.group({
+      usuario: ['',Validators.required],
+      password: ['',Validators.required]
+    })
+  }
+
+  log(){
+    const usuario: Usuario = {
+      nombreUsuario: this.login.value.usuario,
+      password: this.login.value.password
+    }
+
+    console.log(usuario)
+  }
 }
--- src/app/components/inicio/register/register.component.ts
+++ src/app/components/inicio/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -6,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
+  register: FormGroup
 
+  constructor(private fb: FormBuilder) {
+    this.register = this.fb.group({
+      usuario: ['',Validators.required],
+      password: ['',[Validators.required,Validators.minLength(4)]],
+      confirmPassword: ['']
+    }, { validator: this.checkPassword })
+
+  }
+
+  registrarUsuario() : void{
+   
+  }
+
+  checkPassword(group: FormGroup) : any {
+    const pass = group.controls['password'].value
+    const confirmPass = group.controls['confirmPassword'].value
+    return pass === confirmPass ? null : { notSame: true }
+  }
 }
