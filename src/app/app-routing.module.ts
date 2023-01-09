43f4aecc627ad0411171188c42035a1892baa336
joel <libra-marriaga@hotmailt.com>
2023-01-09 12:36:24 -0500

    refactorizacion de rutas y creacion de rutas hijas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BienvenidaComponent } from './components/inicio/bienvenida/bienvenida.component';
+import { InicioComponent } from './components/inicio/inicio.component';
 import { LoginComponent } from './components/inicio/login/login.component';
 import { RegisterComponent } from './components/inicio/register/register.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { CuestionariosComponent } from './components/dashboard/cuestionarios/cuestionarios.component';
+import { CambiarPasswordComponent } from './components/dashboard/cambiar-password/cambiar-password.component';
 
 const routes: Routes = [
-  { path: 'bienvenidos', component: BienvenidaComponent },
-  { path: 'register', component: RegisterComponent },
-  { path: 'login', component: LoginComponent },
-  { path: '', redirectTo: 'bienvenidos', pathMatch: 'full' },
-  { path: '**', redirectTo: 'bienvenidos', pathMatch: 'full' }
+  { path: '', redirectTo: 'inicio', pathMatch: 'full' },
+  { path: 'inicio', component: InicioComponent, children: [
+    { path: '', component: BienvenidaComponent },
+    { path: 'register', component: RegisterComponent },
+    { path: 'login', component: LoginComponent },
+  ]},
+  { path: 'dashboard', component: DashboardComponent, children: [
+    { path: '', component: CuestionariosComponent },
+    { path: 'cambiar-password', component: CambiarPasswordComponent}
+  ] },
+  { path: '**', redirectTo: 'inicio', pathMatch: 'full' }
 ];
 
 @NgModule({
